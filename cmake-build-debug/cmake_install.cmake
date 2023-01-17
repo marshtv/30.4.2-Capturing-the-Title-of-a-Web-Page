0abc7863d62f5419089b6a31b974b2e9ea2dd8ea
marshtv <marshtv.1972@gmail.com>
2023-01-18 01:08:29 +0300

    Connected cpr library

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("F:/__C++/Skillbox/30 Connecting the CPR Library/30.4.2 Capturing the Title of a Web Page/cmake-build-debug/_deps/cpr-build/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
